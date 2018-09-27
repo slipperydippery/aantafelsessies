6f75e3e131fc9847b44ad62a45b753bcab58b8c3
Maarten de Jager <maartendejager@gmail.com>
2018-09-27 16:58:02 +0100

    animatiefilm update

--- resources/views/pages/animatiefilm.blade.php
+++ resources/views/pages/animatiefilm.blade.php
@@ -14,9 +14,9 @@
 
 		        <h4>Schulden voorkomen? Hoe kunnen we dat samen doen?</h4>
 		        <div class="embed-responsive embed-responsive-16by9">
-		          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/nB_28sMNVwk?rel=0" allowfullscreen></iframe>
+		          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/eNQUyFWlTsA?rel=0" allowfullscreen></iframe>
 		        </div>
-		        <a href="https://www.youtube.com/watch?v=nB_28sMNVwk">bekijk de video op youtube</a>				
+		        <a href="https://www.youtube.com/watch?v=eNQUyFWlTsA">bekijk de video op youtube</a>				
 			</div>
 		</div>
 	
